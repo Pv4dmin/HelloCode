aae88c97fe79b8f370fbff5331c6961ca24ea041
Vyacheslav <pvadmin@mail.ru>
2023-08-08 09:18:27 +0800

    Ура это же Маш!!2

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 8;
+int b = 3;
+int c = 38;
+int d = 1;
+int e = 4;
+int max = a;
+if  (a > max) max = a;
+if  (b > max) max = b;
+if  (c > max) max = c;
+if  (d > max) max = d;
+if  (e > max) max = e;
+
+Console.Write("Max = ");
+Console.Write(max);
